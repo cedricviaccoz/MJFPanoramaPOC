ebc9d33346375ea34fbf666566fdf3e507a25c74
Invernizzi Hakim <hakim.invernizzi@epfl.ch>
2018-05-22 09:45:22 +0200

    first version

---
+++ Assets/Stuff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stuff : MonoBehaviour {
+	public GameObject cylinder;
+	float angle;
+
+	// Use this for initialization
+	void Start () {
+		angle = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (UnityEngine.Input.GetKeyDown (KeyCode.Space)) {
+			angle += 45;
+			cylinder.transform.localRotation = Quaternion.Euler (0, angle, 0);
+		}
+	}
+}
